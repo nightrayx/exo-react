f9448e0667d48e9a94a8517f302ab1e9b5fe5d8f
thibaut mathon <thibaut.mathon@hotmail.fr>
2021-02-11 16:21:03 +0100

    test

--- src/App.js
+++ src/App.js
@@ -1,25 +1,122 @@
-import logo from './logo.svg';
 import './App.css';
+import React from'react';
+import Meteo from './components/Meteo'
+import Ville from './components/Ville'
+import'bootstrap/dist/css/bootstrap.min.css';
+import 'weather-icons/css/weather-icons.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const Api_Key = "d3f6aac75e30840a28b779820d6e428a";
+
+
+class App extends React.Component {
+  constructor() {
+    super();
+    this.state={
+      ville: undefined,
+      pays: undefined,
+      icon:undefined,
+      main:undefined,
+      celsius:undefined,
+      temp_max: null,
+      temp_min: null,
+      description: "",
+      error: false
+    };
+    this.weatherIcon = {
+      Eclair: "wi-thunderstorm",
+      Brume: "wi-sleet",
+      Pluie: "wi-storm-showers",
+      Neige: "wi-snow",
+      Atmosphere: "wi-fog",
+      Soleil: "wi-day-sunny",
+      Nuageux: "wi-day-fog"
+    };
+  }
+
+  get_WeatherIcon(icons, rangeId) {
+    switch (true) {
+      case rangeId >= 200 && rangeId < 232:
+        this.setState({ icon: icons.Eclair });
+        break;
+      case rangeId >= 300 && rangeId <= 321:
+        this.setState({ icon: icons.Brume });
+        break;
+      case rangeId >= 500 && rangeId <= 521:
+        this.setState({ icon: icons.Pluie });
+        break;
+      case rangeId >= 600 && rangeId <= 622:
+        this.setState({ icon: icons.Neige });
+        break;
+      case rangeId >= 701 && rangeId <= 781:
+        this.setState({ icon: icons.Atmosphere });
+        break;
+      case rangeId === 800:
+        this.setState({ icon: icons.Soleil });
+        break;
+      case rangeId >= 801 && rangeId <= 804:
+        this.setState({ icon: icons.Nuageux });
+        break;
+      default:
+        this.setState({ icon: icons.Nuageux });
+    }
+  }
+  calCelsius(temp) {
+    let cell = Math.floor(temp - 273.15);
+    return cell;
+  }
+
+  getVille = async e => {
+    e.preventDefault();
+
+    const ville = e.target.elements.ville.value;
+    const pays = e.target.elements.pays.value;
+
+
+ if (pays && ville) {
+      const api_call = await fetch(
+        `http://api.openweathermap.org/data/2.5/weather?q=${ville},${pays}&appid=${Api_Key}`
+      );
+
+      const response = await api_call.json();
+
+      this.setState({
+        ville: `${response.name}, ${response.sys.pays}`,
+        pays: response.sys.pays,
+        main: response.weather[0].main,
+        celsius: this.calCelsius(response.main.temp),
+        temp_max: this.calCelsius(response.main.temp_max),
+        temp_min: this.calCelsius(response.main.temp_min),
+        description: response.weather[0].description,
+        error: false
+      });
+
+      // seting icons
+      this.get_WeatherIcon(this.weatherIcon, response.weather[0].id);
+
+      console.log(response);
+    } else {
+      this.setState({
+        error: true
+      });
+    }
+  };
+
+  render() {
+    return (
+      <div className="App">
+        <Meteo loadweather={this.getVille} error={this.state.error} />
+    
+        <Ville
+          villename={this.state.ville}
+          weatherIcon={this.state.icon}
+          temp_celsius={this.state.celsius}
+          temp_max={this.state.temp_max}
+          temp_min={this.state.temp_min}
+          description={this.state.description}
+        />
+      </div>
+    );
+  }
 }
 
 export default App;
---
+++ src/components/Meteo.jsx
@@ -0,0 +1,46 @@
+
+import React from "react";
+import "../App.css";
+
+const Form = props => {
+  return (
+    <div className="container h-100">
+      <form onSubmit={props.loadweather}>
+        <div>{props.error ? error() : ""}</div>
+        <div className="row">
+          <div className="col-md-3 offset-md-2">
+            <input
+              type="text"
+              className="form-control"
+              placeholder="ville"
+              name="ville"
+              autoComplete="off"
+            />
+          </div>
+          <div className="col-md-3">
+            <input
+              type="text"
+              className="form-control"
+              placeholder="pays"
+              name="pays"
+              autoComplete="off"
+            />
+          </div>
+          <div className="col-md-3 mt-md-0 mt-2 text-md-left ">
+            <button className="btn btn-warning">Meteo</button>
+          </div>
+        </div>
+      </form>
+    </div>
+  );
+};
+
+const error = props => {
+  return (
+    <div className="alert alert-danger mx-5" role="alert">
+      Recherche ville et pays 
+    </div>
+  );
+};
+
+export default Form;
